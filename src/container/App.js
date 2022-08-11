9cb90ecce4cc182478216a38321c3057c5d40ef3
marinazv <marinazuritaverzini@gmail.com>
2022-08-11 20:03:13 -0300

    probando rama

--- src/container/App.js
+++ src/container/App.js
@@ -5,16 +5,7 @@ import FunctionComponent from '../components/FunctionComponent';
 
 function App() {
   return (    
-    <div className="App">
-        <h3>Invitados:</h3>
-          <ClassComponent nombre="Nicolas" estaEnLista={true} />
-          <ClassComponent nombre="Ivan" estaEnLista={false} />
-          <ClassComponent nombre="Carolina" estaEnLista={true} />
-        <h3>Tareas: </h3>
-          <FunctionComponent nombre="Nicolas" tarea="papas fritas" />
-          <FunctionComponent nombre="Ivan" tarea="pizzas" />
-          <FunctionComponent nombre="Carolina" tarea="bebidas" />
-    </div>
+
   )
 }
 
